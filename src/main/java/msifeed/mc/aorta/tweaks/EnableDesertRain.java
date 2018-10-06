33ee740466d5d094e98e32c588d3e7da0e1d512e
msifeed <remsifeed@gmail.com>
2018-10-07 01:27:27 +0300

    Добавим еще больше дождей

--- src/main/java/msifeed/mc/aorta/tweaks/EnableDesertRain.java
+++ src/main/java/msifeed/mc/aorta/tweaks/EnableDesertRain.java
@@ -2,23 +2,21 @@ package msifeed.mc.aorta.tweaks;
 
 import cpw.mods.fml.relauncher.ReflectionHelper;
 import net.minecraft.launchwrapper.Launch;
-import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.biome.BiomeGenDesert;
-import net.minecraft.world.biome.BiomeGenSavanna;
+import net.minecraft.world.biome.*;
 
 public class EnableDesertRain {
     public static void apply() {
         final String rainField = (Boolean) Launch.blackboard.getOrDefault("fml.deobfuscatedEnvironment", false)
                 ? "enableRain" : "field_76765_S";
         for (BiomeGenBase biome : BiomeGenBase.getBiomeGenArray()) {
-            if (isDesertBiome(biome)) {
+            if (biome != null && !isNoRainBiome(biome)) {
                 ReflectionHelper.setPrivateValue(BiomeGenBase.class, biome, true, rainField);
                 biome.rainfall = 0.4f; // Plains value
             }
         }
     }
 
-    private static boolean isDesertBiome(BiomeGenBase biome) {
-        return biome instanceof BiomeGenDesert || biome instanceof BiomeGenSavanna;
+    private static boolean isNoRainBiome(BiomeGenBase biome) {
+        return biome instanceof BiomeGenHell || biome instanceof BiomeGenEnd;
     }
 }
