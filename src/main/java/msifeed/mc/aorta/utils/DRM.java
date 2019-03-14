7798da92f74ef573f8b65f168a92fcea5ca22f84
msifeed <remsifeed@gmail.com>
2019-03-14 22:18:11 +0300

    Сетевые фильтры

--- src/main/java/msifeed/mc/aorta/utils/DRM.java
+++ src/main/java/msifeed/mc/aorta/utils/DRM.java
@@ -11,11 +11,17 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum  DRM {
     INSTANCE;
 
-    private static final InetAddress SERVER = InetAddresses.forString("149.202.87.159");
+    private static final Set<InetAddress> SERVERS = Stream.of("137.74.214.123", "79.137.9.233")
+            .map(s -> InetAddresses.forString(s))
+            .collect(Collectors.toSet());
 
     public static void apply() {
         MinecraftForge.EVENT_BUS.register(INSTANCE);
@@ -27,7 +33,7 @@ public enum  DRM {
         if (!(socket instanceof InetSocketAddress)) return;
 
         final InetAddress address = ((InetSocketAddress) socket).getAddress();
-        if (address.isLoopbackAddress() || address.equals(SERVER)) return;
+        if (address.isLoopbackAddress() || SERVERS.contains(address)) return;
 
         if (Desktop.isDesktopSupported()) {
             try {
