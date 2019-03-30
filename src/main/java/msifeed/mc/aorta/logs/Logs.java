4b2cae800f6e25c3d98350f8fa93507973dfc629
msifeed <remsifeed@gmail.com>
2019-03-31 00:31:33 +0300

    Логирование входа/выхода игроков

--- src/main/java/msifeed/mc/aorta/logs/Logs.java
+++ src/main/java/msifeed/mc/aorta/logs/Logs.java
@@ -1,6 +1,8 @@
 package msifeed.mc.aorta.logs;
 
 import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 import net.minecraft.command.ICommandSender;
 import net.minecraftforge.common.MinecraftForge;
 import org.apache.logging.log4j.LogManager;
@@ -13,12 +15,26 @@ public enum Logs {
     private static DBHandler dbHandler = new DBHandler();
 
     public static void init() {
-        if (FMLCommonHandler.instance().getSide().isServer())
+        if (FMLCommonHandler.instance().getSide().isServer()) {
             MinecraftForge.EVENT_BUS.register(dbHandler);
+            FMLCommonHandler.instance().bus().register(INSTANCE);
+        }
     }
 
     public static void log(ICommandSender sender, String type, String message) {
         if (FMLCommonHandler.instance().getSide().isServer())
             dbHandler.logCommand(sender, type, message);
+    }
+
+    @SubscribeEvent
+    public void onPlayerJoined(PlayerEvent.PlayerLoggedInEvent event) {
+        if (!event.player.worldObj.isRemote)
+            log(event.player, "login", "[login]");
+    }
+
+    @SubscribeEvent
+    public void onPlayerLeft(PlayerEvent.PlayerLoggedOutEvent event) {
+        if (!event.player.worldObj.isRemote)
+            log(event.player, "logout", "[logout]");
     }
 }
