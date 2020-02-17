fbcbab7e3b0e2b9bd0377ca863754a5e908f015a
AleXXX_007 <AleXXX_007@inbox.ru>
2020-02-17 18:02:59 +0400

    Конфиги для предметов

---
+++ src/main/java/msifeed/mc/more/content/ItemDefines.java
@@ -0,0 +1,9 @@
+package msifeed.mc.more.content;
+
+public class ItemDefines {
+    public int convictionIndictSin = 1;
+    public int forgivnessStoneSin = 1;
+    public int gifterOfferingEstitence = 1;
+    public int soulGrabSin = 1;
+    public int soulGrabEstitence = 1;
+}
--- src/main/java/msifeed/mc/more/content/ItemGifterOffering.java
+++ src/main/java/msifeed/mc/more/content/ItemGifterOffering.java
@@ -3,6 +3,7 @@ package msifeed.mc.more.content;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import msifeed.mc.Bootstrap;
 import msifeed.mc.genesis.GenesisCreativeTab;
+import msifeed.mc.more.More;
 import msifeed.mc.more.crabs.character.Character;
 import msifeed.mc.more.crabs.utils.CharacterAttribute;
 import msifeed.mc.more.crabs.utils.Differ;
@@ -38,6 +39,7 @@ public class ItemGifterOffering extends Item {
             final EntityPlayer target = (EntityPlayer) event.target;
             final Character playerAfter = CharacterAttribute.require(player);
             final Character playerBefore = new Character(playerAfter);
+            final ItemDefines config = More.DEFINES.get().items;
 
             if (playerAfter.estitence <= 10) {
                 player.addChatMessage(new ChatComponentText(L10n.fmt("more.est.gift_not_enough")));
@@ -52,8 +54,8 @@ public class ItemGifterOffering extends Item {
                 return;
             }
 
-            playerAfter.estitence--;
-            targetAfter.estitence++;
+            playerAfter.estitence = Math.max(playerAfter.estitence - config.gifterOfferingEstitence, 10);
+            targetAfter.estitence = Math.min(targetAfter.estitence + config.gifterOfferingEstitence, 90);
             targetAfter.illness.illness = 0;
             targetAfter.illness.limit = 0;
             targetAfter.illness.treatment = 0;
