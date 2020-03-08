b05d4d81c7f3fa067fd7641f349bdc0834fbfb16
AleXXX_007 <AleXXX_007@inbox.ru>
2020-03-08 09:14:50 +0400

    Исправления поля ввода

--- src/main/java/msifeed/mc/more/client/morph/EditParamsView.java
+++ src/main/java/msifeed/mc/more/client/morph/EditParamsView.java
@@ -15,7 +15,7 @@ class EditParamsView extends Widget {
         estitenceInput.getSizeHint().x = 30;
         estitenceInput.setText(String.valueOf(character.estitence));
         estitenceInput.setFilter(s -> TextInput.isUnsignedIntBetween(s, 1, 100));
-        estitenceInput.setCallback(s -> character.estitence = (byte) Math.max(estitenceInput.getInt(), 20));
+        estitenceInput.setCallback(s -> character.estitence = estitenceInput.getInt());
         addChild(estitenceInput);
 
         addChild(new Label("Sin"));
@@ -23,7 +23,7 @@ class EditParamsView extends Widget {
         sinInput.getSizeHint().x = 30;
         sinInput.setText(String.valueOf(character.sin));
         sinInput.setFilter(s -> TextInput.isUnsignedIntBetween(s, -1, 100));
-        sinInput.setCallback(s -> character.sin = (byte) sinInput.getInt());
+        sinInput.setCallback(s -> character.sin = sinInput.getInt());
         addChild(sinInput);
     }
 }
