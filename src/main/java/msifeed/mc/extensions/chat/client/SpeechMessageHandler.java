9338c4fc54b96b3565b974f69b593b0641782185
msifeed <remsifeed@gmail.com>
2020-04-11 13:38:01 +0300

    Звук речевых сообщений в чате

--- src/main/java/msifeed/mc/extensions/chat/client/SpeechMessageHandler.java
+++ src/main/java/msifeed/mc/extensions/chat/client/SpeechMessageHandler.java
@@ -7,6 +7,9 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import msifeed.mc.extensions.chat.ChatMessage;
 import msifeed.mc.extensions.chat.composer.Composer;
+import msifeed.mc.extensions.chat.composer.SpeechType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IChatComponent;
 
@@ -14,10 +17,19 @@ public class SpeechMessageHandler implements IMessageHandler<ChatMessage, IMessa
     @SideOnly(Side.CLIENT)
     @Override
     public IMessage onMessage(ChatMessage message, MessageContext ctx) {
-        final EntityPlayer player = net.minecraft.client.Minecraft.getMinecraft().thePlayer;
+        final EntityPlayer player = Minecraft.getMinecraft().thePlayer;
         if (Composer.canReceiveMessage(player, message)) {
             final IChatComponent chatComponent = Composer.formatMessage(player, message);
             player.addChatMessage(chatComponent);
+
+            if (message.type == SpeechType.SPEECH) {
+                final Entity speaker = player.worldObj.getEntityByID(message.senderId);
+                if (speaker != null) {
+                    final float distance = player.getDistanceToEntity(speaker);
+                    final float volume = (1 - distance / message.radius) + 0.4f;
+                    player.playSound("random.pop", volume, 0.7F);
+                }
+            }
         }
         return null;
     }
